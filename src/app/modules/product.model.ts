fbe2431a20ed95a55e3117fe094b910a7d2f5a82
Khalad0007 <www.arifhossain49919@gmail.com>
2024-06-25 00:45:07 +0800

    Created Product model

---
+++ src/app/modules/product.model.ts
@@ -0,0 +1,35 @@
+import { Schema, model } from "mongoose";
+import { Product } from "./product/product.interface";
+
+const productSchema = new Schema<Product>({
+  id: { type: String },
+  description: { type: String },
+  category: { type: String },
+  tags: {
+    type: String,
+    enum: ["smartphone", "Apple", "iOS"],
+    messasge: "smartphone, apple , ios is required",
+  },
+  variants: {
+    type: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+  inventory: {
+    quantity: {
+      type: String,
+      required: true,
+    },
+    inStock: {
+      type: Boolean,
+      required: true,
+    },
+  },
+});
+
+const Product = model<Product>("Product", productSchema);
